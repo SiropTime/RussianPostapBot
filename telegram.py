4e4298b70c0ad5e2573ca430b20869069a659375
KlenoviySirop <55997526+KlenoviySirop@users.noreply.github.com>
2022-02-28 20:56:26 +0300

    Создание бота и вводного сообщения

--- telegram.py
+++ telegram.py
@@ -1,22 +1,45 @@
-import telebot as tb
+from aiogram import Bot, types
+from aiogram.dispatcher import Dispatcher
+from aiogram.utils import executor
 from game import *
 from game_utils import *
 
-bot = tb.TeleBot("5218155011:AAF0GYeUQtMswyMXHhsULlrLHlnQrphEpA8")
+TOKEN = "5218155011:AAF0GYeUQtMswyMXHhsULlrLHlnQrphEpA8"
+ADMIN = 390919747
+bot = Bot(token=TOKEN)
+dp = Dispatcher(bot)
+player = Player()
+game = Game()
+
+kb = types.ReplyKeyboardMarkup(resize_keyboard=True)
+kb.add(types.InlineKeyboardButton(text="Создать персонажа"))
 
 
-@bot.message_handler(content_types=['text'])
-def get_text_messages(message):
-    if message.text == "/start":
-        bot.send_message(message.chat.id, "Маслину словил " + str(message.chat.id))
-        player = Player()
-        player.id = int(message.chat.id)
+def check_player(id: int) -> bool:
+    res = False
+    for i in range(len(game.players)):
+        if id == game.players[i].id:
+            res = True
+            return res
+        else:
+            res = False
 
-@bot.message_handler(commands=['create_player'])
-def create_player(message):
-    bot.send_message(message.chat.id, "Создание игрока")
+
+@dp.message_handler(commands=['start'])
+async def send_welcome(msg: types.Message):
+    if check_player(msg.from_user.id):
+        await msg.answer("""
+                        Здравствуй!
+                        Этот бот предназначен для помощи в отыгрыше Словесной РПГ.
+                        Сейчас нам нужно создать твоего персонажа, введи своё имя!
+                        P.S. Если у тебя уже есть персонаж, введи команду /return
+                        """)
+    player.id = msg.from_user.id
+
+@dp.message_handler(commands=['return'])
+async def return_player(msg: types.Message):
+    pass
 
 
 if __name__ == "__main__":
-    game = Game()
-    bot.polling(none_stop=True)
+    executor.start_polling(dp)
