36b625aa5124876ef82e257766775c4dd0a9d9d4
KlenoviySirop <55997526+KlenoviySirop@users.noreply.github.com>
2022-03-08 00:05:14 +0300

    Расчёт доп навыков пользователем (без баланса), бесфункциональное меню Фикс

--- telegram.py
+++ telegram.py
@@ -160,10 +160,10 @@ async def process_add_skills(callback_query: types.CallbackQuery):
             if len(priority_skills) >= 5:
                 await callback_query.answer(text="Вы уже выбрали 5 навыков! Уберите уже выбранные для изменения!", show_alert=True)
             else:
-                button_list[callback_query.data].text += " ☑"
+                button_list[callback_query.data].text = "☑ " + button_list[callback_query.data].text
                 priority_skills.append(callback_query.data)
         elif callback_query.data in priority_skills:
-            button_list[callback_query.data].text = button_list[callback_query.data].text[:-2]
+            button_list[callback_query.data].text = button_list[callback_query.data].text[2:]
             priority_skills.remove(callback_query.data)
         elif callback_query.data == "end":
             if len(priority_skills) < 5:
